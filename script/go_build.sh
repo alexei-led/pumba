3c669a448ba6f5b015054df96155851671f53293
Alexei Ledenev <alexei.led@gmail.com>
2016-10-30 14:52:35 +0200

    fix static link

--- script/go_build.sh
+++ script/go_build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 [ -z "$DIST" ] && DIST=.dist
-CGO_ENABLE=0
 
 go_build() {
   [ -d "${DIST}" ] && rm -rf "${DIST}/*"
   [ -d "${DIST}" ] || mkdir -p "${DIST}"
   glide install
-  go build -v -o "${DIST}/pumba"
+  CGO_ENABLED=0 go build -v -o "${DIST}/pumba"
 }
 
 go_build
