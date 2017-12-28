0816995db83aed0d448da4418770c64234098a42
Michael Grosser <michael@grosser.it>
2017-12-28 09:29:37 -0800

    use dumb-init and su-exec (#71)

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -5,7 +5,7 @@ if [ "$1" = "pumba" ]; then
   if [ -S /var/run/docker.sock ]; then
     chown -R pumba:pumba /var/run/docker.sock
   fi
-  exec gosu pumba:pumba "$@"
+  exec su-exec pumba:pumba "$@"
 fi
 
 exec "$@"
