e40588c02b4ddb468bab87c6a686bb33ec22b7f2
Alexei Ledenev <alexei.led@gmail.com>
2018-04-01 09:28:08 +0300

    regenerating mock

--- hack/mockgen.sh
+++ hack/mockgen.sh
@@ -4,4 +4,4 @@
 go get -u github.com/vektra/mockery/.../
 
 # (re)generate mock for Docker APIClient interface
-mockery -name APIClient -dir vendor/github.com/docker/docker/client -output ./container/mocks
+mockery -name APIClient -dir vendor/github.com/docker/docker/client -output ./pkg/container/mocks
